027eeaed6de75f43ebbe446e366f46d1079c1e59
Vladimir Canic <vladimirdavincijevkod@gmail.com>
2020-08-17 14:09:50 +0200

    SUBJECT: Function that checks Intensity Level
    PROBLEM:
    Reason for this commit is function for checking the intensity level of an imaage.
    Function for given intensity level returns information about its correctness.
    
    SOLUTION:
    	- Add Implementation
    	- Add function documentation
    
    NOTE:

--- week1/intensity_level/intensity_level.py
+++ week1/intensity_level/intensity_level.py
@@ -2,7 +2,28 @@ import numpy as np
 import cv2
 
 
-def intensity_level():
+def check_intensity_level(level):
+	""" 
+	Intensity level must in [2, 128], and must be power of number two.
+	This function checks that condition.
+
+	Args:
+		level (int): Intensity level.
+
+	Returns:
+		validity (bool): Intensity level is correct or not.
+
+	Examples:
+		>>> check_intensity_level(64)
+		True
+		>>> check_intensity_level(67)
+		False
+
+	"""
+	return level in map(lambda x: 2 ** x, range(1, 8))
+
+
+def intensity_level(image, level):
 	pass
 
 
