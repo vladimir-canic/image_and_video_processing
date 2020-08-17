3686d304495b42b31f986bff824fdc851b6fd518
Vladimir Canic <vladimirdavincijevkod@gmail.com>
2020-08-17 20:51:35 +0200

    SUBJECT: Initialization of Unittests for Intensity Level
    PROBLEM:
    Initialization of tests.py file. This tests are used for reducing intensity level exercise.
    
    SOLUTION:
    	- Init Class CheckIntensityLevel
    	- Init Class IntensityLevel
    
    NOTE:

---
+++ week1/intensity_level/tests.py
@@ -0,0 +1,27 @@
+import unittests
+
+
+class CheckIntensityLevel(unittests.Test):
+
+	def setUp(self):
+		pass
+
+
+	def tearDown(self):
+		pass
+
+
+
+class IntensityLevel(unittests.Test):
+
+	def setUp(self):
+		pass
+
+
+	def tearDown(self):
+		pass
+
+
+
+if __name__ == "__main__":
+	unittests.main()
