d1bbb5b5c428e95aaf3ae995fa6c4234bbcf1165
Vladimir Canic <vladimirdavincijevkod@gmail.com>
2020-08-17 18:04:19 +0200

    SUBJECT: Implementation of the Function for Intensity Level
    PROBLEM:
    Implementation of the function for intensity level.
    
    SOLUTION:
    	- Checking the Intensity Level
    	- Reading an Image
    	- Changing the Intensity Level
    	- Writing the Image
    
    NOTE:

--- week1/intensity_level/intensity_level.py
+++ week1/intensity_level/intensity_level.py
@@ -24,7 +24,54 @@ def check_intensity_level(level):
 
 
 def intensity_level(image, level):
-	pass
+	"""
+	Change intensity level for given image according to level argument. 
+
+	Args:
+		image (str, numpy.ndarray): Image can be represented as numpy 
+					n-dimensional array or string that represents full path
+					to the image.
+		level (int): Intensity level.
+
+	Returns:
+		image (numpy.ndarray): The function returns numpy ndarray
+							   if input image is numpy ndarray.
+		flag (bool): The function returns correctnes flag if input image
+					 is represented as string path to it.
+
+	Raises:
+		ValueError: If intensity level doesn't have correct value.
+		TypeError: If input image doesn't have correct type.
+
+	Examples:
+		>>> intensity_level(np.array([[234, 52], [12, 147]]), 64)
+		[[56, 13], [3, 36]]
+
+	"""
+
+	if not check_intensity_level(level):
+		raise ValueError("Invalid intensity level. It must be integer between 2 and 128. ")
+
+	# Read image
+	if type(image) == str:
+		img = cv2.imread(image)
+	elif type(image) == np.ndarray:
+		img = image
+	else:
+		raise TypeError("Type of image must string that represents path to the image."
+						"Or numpy n-dimensional array. ")
+
+	# Change Intensity Level
+	img /= (256 / level)
+	img = img.astype(np.int8)
+
+	# Write image
+	if type(image) == str:
+		name, extension = image.split('.')
+		cv2.imwrite(name + "_out." + extension, img)
+		return True	
+	
+	return img
 
 
 def main():
